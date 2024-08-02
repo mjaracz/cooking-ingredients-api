366ecf92689408bb48c9f2a4fd2d90bb4c4fad24
michal19996@gmail.com <michal19996@gmail.com>
2024-08-02 21:00:06 +0200

    feat: create, update unit test for cooking-ingredients.controller file in cooking-ingredients microservice

--- apps/cooking-ingredients/src/tests/cooking-ingredients.controller.spec.ts
+++ apps/cooking-ingredients/src/tests/cooking-ingredients.controller.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CookingIngredientsController } from './cooking-ingredients.controller';
-import { CookingIngredientsService } from './cooking-ingredients.service';
+import { CookingIngredientsController } from '../cooking-ingredients.controller';
+import { CookingIngredientsService } from '../cooking-ingredients.service';
+
+export class CookingIngredientsServiceMock {
+  async getAllRecipes() {
+    return ['mock list of recipes'];
+  }
+} 
 
 describe('CookingIngredientsController', () => {
   let cookingIngredientsController: CookingIngredientsController;
@@ -8,15 +14,22 @@ describe('CookingIngredientsController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [CookingIngredientsController],
-      providers: [CookingIngredientsService],
+      providers: [
+        {
+          useClass: CookingIngredientsServiceMock,
+          provide: CookingIngredientsService,
+        }
+      ],
     }).compile();
 
     cookingIngredientsController = app.get<CookingIngredientsController>(CookingIngredientsController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(cookingIngredientsController.getHello()).toBe('Hello World!');
+  describe('getAllRecipes()', () => {
+    it('should return array of recipes', () => {
+      return cookingIngredientsController.getAllRecipes().then(recipes => {
+        expect(recipes).toEqual(['mock list of recipes']);
+      })
     });
   });
 });
